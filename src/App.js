fa33ae7e2b31fc38f704e8a3b88dc61f10d208bd
mhd swalih <MuhammedSwalih034@gmail.com>
2023-12-26 10:44:01 +0530

    complited

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import { useState } from "react";
 import './App.css'
+import { useState } from 'react';
 
 
 function App() {
-  const [ToDos,SetToDos]=useState([])
-  const [ToDo,SetToDo]=useState('')
-
+ 
+  const [todos,Settodos] = useState([])
+  const [todo,settodo] = useState("")
 
   
   
@@ -13,29 +13,55 @@ function App() {
     <div>
     <div className="todo-container">
     <h1>ToDo List</h1>
+    
     <div className="add-task">
-        <input value={ToDo} onChange={(e)=>SetToDo(e.target.value)}   type="text" id="newTask" placeholder="Add a new task"/> 
-
-        <button onClick={()=>SetToDos([...ToDos,{id:Date.now() ,text:ToDo , stutus:false }])}>Add</button>
-
-    </div>
-    <div>{
-      ToDos.map((obj)=>{
-        return(
-        <div key={obj.id} > <ul  className="taskList">{obj.text}</ul>
-
-
-        <input type="checkbox" className="checkbox "></input>
-        
+    
+       <input value={todo} onChange={(event)=>settodo(event.target.value)} type="text" id="newTask" placeholder="Add a new task"/> 
+    
+        <button onClick={()=>{Settodos([...todos,{id:Date.now(),text:todo ,statuss:false}])}} >Add</button>
         </div>
-       )
-
+        </div>
+    
+    
+    
+    <div className='sg'>
+      {todos.map((valu)=>{
         
-    })}
-    </div>
-    </div>
-    </div>
-  );
-}
+
+
+        return(<div>
+          <input value={valu.statuss} key={valu.id} onChange={(e)=>{console.log(e.target.checked)
+            console.log(valu)
+          Settodos(todos.filter(obj2=>{if (valu.id ===obj2.id){
+            obj2.statuss=e.target.checked
+            }
+            return obj2}))
+                  }}
+          type="checkbox"   className='right'/>
+          <h1>{valu.text}</h1>
+
+          
+</div>
+          
+        )})}
+<div className='activetodo'>
+  <h1>active todo</h1>
+        {todos.map((obj)=>{
+          if (obj.statuss){
+           return <div className='active todo'>
+            <p>{obj.text}</p></div>
+          }
+          return null
+
+        })}
+        </div>
+       
+       </div>
+       </div>)}
+     
+   
+    
+  
+
    
 export default App;
